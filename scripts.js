843950a6b7ee179905ace9375e55a0e16bcc0716
kor-2 <checkmann.corentin@gmail.com>
2022-02-28 16:49:02 +0100

    version 2.0

--- scripts.js
+++ scripts.js
@@ -1,136 +1,84 @@
+const base = document.querySelector("#base")
+const mains = document.querySelectorAll(".mains")
+const scoreJ = document.querySelector('#joueurScore')
+const scoreC = document.querySelector('#ordiScore')
+const scoreD = document.querySelector('#drawScore')
+const showOrdi = document.querySelector('#ordi')
+const reset = document.querySelector('#reset')
+
+
 function shuffle(table){
     let rand = table
-    let i = table.length, k, temp
+    let i = table.length, k
     while (--i > 0) {
         k = Math.floor(Math.random() * (i + 1))
         return temp = rand[k]
     }
-    
 }
 
-let comp = ['paper','rock','scissor'];
+let scoreJoueur = 0
+let scoreOrdi = 0
+let scoreDraw = 0
 
+scoreJ.innerHTML = "<p>Score joueur :</p>"
+scoreC.innerHTML = "<p>Score ordi :</p>"
+scoreD.innerHTML = "<p>Egalité  :</p>"
+base.innerHTML = "<p>Joueur </p> <p> VS </p> <p>  Ordi </p><p>Résultat</p>"
 
-let chose = document.querySelector('#chose')
-
-let rock = document.createElement("div")
-rock.classList.add("rock")
-rock.classList.add("tool")
-chose.appendChild(rock)
-rock.innerHTML = "<i class=\"fa-solid fa-mountain\"></i>"
-
-let paper = document.createElement("div")
-paper.classList.add("paper")
-paper.classList.add("tool")
-chose.appendChild(paper)
-paper.innerHTML = "<i class=\"fa-solid fa-paper-plane\"></i>"
-
-let scissor = document.createElement("div")
-scissor.classList.add("scissor")
-scissor.classList.add("tool")
-chose.appendChild(scissor)
-scissor.innerHTML = "<i class=\"fa-solid fa-scissors\"></i>"
-
-let pcReply = document.querySelector('#reply')
-let reply = document.createElement("div")
-reply.classList.add("reply")
-pcReply.appendChild(reply)
-
-let tool = document.querySelector('.tool')
-reply.innerHTML = "<p>Salut</p>"
-
-
-
-
-let score = document.querySelector('#score')
-
-let boxJ = document.createElement("div")
-boxJ.classList.add("boxJ")
-score.appendChild(boxJ)
-boxJ.innerText = ""
-
-let boxC = document.createElement("div")
-boxC.classList.add("boxC")
-score.appendChild(boxC)
-
-let jScore = 1 
-let compScore = 1
-
-
-rock.addEventListener("click", function(){
-
-    let rand = shuffle(comp)
+function comparer(j, o){
     
-
-
-    if (rand == "paper") {
-        reply.innerHTML = "<i class=\"fa-solid fa-paper-plane\"></i>"
-        boxC.innerText = "Score Ordinateur : "+ compScore++
-
-
+    const options = {
+    "Pierre": {"Pierre" : "Egalité", "Feuille" : "Perdu", "Ciseaux" : "Victoire"},
+    
+    "Feuille" : {"Pierre" : "Victoire", "Feuille" : "Egalité", "Ciseaux" : "Perdu"},
+    
+    "Ciseaux" : {"Pierre" : "Perdu", "Feuille" : "Victoire", "Ciseaux" : "Egalité"}
+    
     }
-    else if (rand == "scissor") {
-        reply.innerHTML = "<i class=\"fa-solid fa-scissors\"></i>"
-        boxJ.innerText = "Score Joueur :"+ jScore++
-    }else{
-        reply.innerHTML = "<i class=\"fa-solid fa-mountain\"></i>"
-
+    
+    switch (options[j][o]) {
+        case "Victoire":
+            result = " Victoire Joueur " 
+            base.innerHTML = "<p>Score joueur : " + joueur + ' </p> <p>VS</p> <p> ' + " Ordi : " + ordi + " </p><p class=\" green\"> " + result + "</p>"
+            scoreJoueur++
+            scoreJ.innerHTML = "<p>Joueur score :" + scoreJoueur + "</p>"
+        break;
+        
+        case "Perdu":
+            result = "Defaite Joueur"
+            base.innerHTML = "<p>Joueur : " + joueur + ' </p> <p>VS</p> <p> ' + " Ordi  : " + ordi + " </p><p class=\" red\"> " + result + "</p>"
+            scoreOrdi++
+            scoreC.innerHTML = "<p>Score ordi :" + scoreOrdi + "</p>"
+            break;
+        case "Egalité" :
+            result = " Egalité " 
+            base.innerHTML = "<p>Joueur : " + joueur + ' </p> <p>VS</p> <p> ' + " Ordi : " + ordi + " </p><p> " + result + "</p>"
+            scoreDraw++
+            scoreD.innerHTML = "<p>Egalité  :" + scoreDraw + "</p>"
     }
+}
+        
+optionsOrdi = ['Pierre', 'Feuille', 'Ciseaux']
 
+        
+
+
+mains.forEach(function(main){
+            
+    main.addEventListener("click", function(){
+        
+
+
+            
+        joueur = main.dataset.choice
+        ordi = shuffle(optionsOrdi)
+        console.log(ordi)
+        comparer(joueur, ordi)
+        
+        
+    })
 })
 
-
-paper.addEventListener("click", function(){
-    let rand = shuffle(comp)
-    
-
-
-    if (rand == "scissor") {
-        reply.innerHTML = "<i class=\"fa-solid fa-scissors\"></i>"
-        boxC.innerText = "Score Ordinateur : "+ compScore++
-        console.log("perdu mon reuf")
-    }
-
-
-    else if (rand == "rock") {
-        reply.innerHTML = "<i class=\"fa-solid fa-mountain\"></i>"
-        console.log("gg mon reuf")
-        boxJ.innerText = "Score Joueur : "+ jScore++
-    }
-    
-    else{
-        reply.innerHTML = "<i class=\"fa-solid fa-paper-plane\"></i>"
-        console.log("draw")
-    }
-
+reset.addEventListener("click", function(){
+    location.reload()
 })
-
-
-scissor.addEventListener("click", function(){
-    let rand = shuffle(comp)
-    
-    
-
-    if (rand == "rock") {
-        reply.innerHTML = "<i class=\"fa-solid fa-mountain\"></i>"
-        boxC.innerText = "Score Ordinateur : "+ compScore++
-    }
-
-
-    else if (rand == "paper") {
-        reply.innerHTML = "<i class=\"fa-solid fa-paper-plane\"></i>"
-
-        boxJ.innerText = "Score Joueur : "+ jScore++
-    }
-    
-    
-    else{
-        reply.innerHTML = "<i class=\"fa-solid fa-scissors\"></i>"
-    }
-})
-console.log(jScore)
-console.log(compScore)
-
-
-
-
